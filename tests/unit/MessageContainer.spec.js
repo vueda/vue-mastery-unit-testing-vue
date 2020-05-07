8f8f4d8bcaefb73a8e50f70edd372905ebc13ee7
Vinícius Ueda <vnsueda@gmail.com>
2020-05-06 22:33:44 -0300

    Vue Mastery Unit Testing - Stub Components

---
+++ tests/unit/MessageContainer.spec.js
@@ -0,0 +1,15 @@
+import MessageContainer from '@/components/MessageContainer'
+import { mount } from '@vue/test-utils'
+
+describe('MessageContainere', () => {
+  it('Wraps MessageDisplayComponent', () => {
+    const wrapper = mount(MessageContainer, {
+      stubs: {
+        MessageDisplay: '<p data-testid="message">Hello from the db!</p>'
+      }
+    })
+
+    const message = wrapper.find('[data-testid="message"]').element.textContent
+    expect(message).toEqual('Hello from the db!')
+  })
+})
